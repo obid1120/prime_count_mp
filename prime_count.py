dc29ac4b4eba82b26acb8b226e8dd1fb4b434bdf
obid1120 <yoqubovobidjon99@gmail.com>
2024-02-13 14:20:42 +0500

    I changed from normal function to multiprocessing function

--- prime_count.py
+++ prime_count.py
@@ -1,10 +1,12 @@
 from math import sqrt
+from time import perf_counter
+from multiprocessing import Process
 
 
 def prime_count(m: int, n: int):
-    
+
     m = m + 1 if m % 2 == 0 else m
-    ls = []
+    # ls = []
     s = 0
 
     for i in range(m, n, 2):
@@ -12,15 +14,28 @@ def prime_count(m: int, n: int):
             if i % j == 0:
                 break
         else:
-            ls.append(i)
+            # ls.append(i)
             s += 1
-    if ls[0] == 1:
-        ls[0] = 2
-    print(s)
-    print(ls)
+    # if ls[0] == 1:
+    #     ls[0] = 2
+    print(f"In interval [{m}-{n}] has {s} prime count")
+    # print(ls)
 
 
 if __name__ == "__main__":
-    m = int(input("m: "))
-    n = int(input("n: "))
-    prime_count(m, n)
+
+    processes = []
+    start = perf_counter()
+    for i in range(1, 10_000_001, 2_500_000):
+        process = Process(target=prime_count, args=[i, i+2_499_999])
+        processes.append(process)
+        process.start()
+
+    for j in processes:
+        j.join()
+    end = perf_counter()
+    print(f"Runtime: {end-start}")
+
+    # m = int(input("m: "))
+    # n = int(input("n: "))
+    # prime_count(m, n)
